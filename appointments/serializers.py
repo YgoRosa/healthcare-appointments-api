bfe8ce5cd5cb29ffc807159283d6d239c8ac6b9b
YgoRosa <ygor18rosa@gmail.com>
2026-06-02 15:43:57 -0300

    feat: add serializer validations

--- appointments/serializers.py
+++ appointments/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profissional, Consulta
+from django.utils import timezone
 
 
 # class ProfissionalSerializer(serializers.ModelSerializer):
@@ -15,13 +16,35 @@ class ProfissionalSerializer(serializers.ModelSerializer):
                 "Nome social deve ter pelo menos 3 caracteres."
             )
         return value
+    
+    def validate_profissao(self, value):
+        if len(value.strip()) < 3:
+            raise serializers.ValidationError(
+                "Profissão deve ter pelo menos 3 caracteres."
+            )
+        return value
 
+    def validate_contato(self, value):
+        if len(value.strip()) < 8:
+            raise serializers.ValidationError(
+                "Contato inválido."
+            )
+        return value
+    
     class Meta:
         model = Profissional
         fields = "__all__"
 
 
 class ConsultaSerializer(serializers.ModelSerializer):
+
+    def validate_data(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError(
+                "Não é possível agendar consultas no passado."
+            )
+        return value
+
     class Meta:
         model = Consulta
         fields = "__all__"
