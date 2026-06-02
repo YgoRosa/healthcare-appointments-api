6b9dd9210e6033a4cb84b099a37eb16759967159
YgoRosa <ygor18rosa@gmail.com>
2026-06-02 13:49:36 -0300

    feat: implement professional crud endpoints

--- appointments/views.py
+++ appointments/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Profissional, Consulta
+from .serializers import ProfissionalSerializer, ConsultaSerializer
+
+
+class ProfissionalViewSet(viewsets.ModelViewSet):
+    queryset = Profissional.objects.all()
+    serializer_class = ProfissionalSerializer
+
+
+class ConsultaViewSet(viewsets.ModelViewSet):
+    queryset = Consulta.objects.all()
+    serializer_class = ConsultaSerializer
